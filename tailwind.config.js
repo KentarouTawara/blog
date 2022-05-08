a816f7b3bba9611a4f20521b6ca52ae0abc4e203
KentarouTawara <sironagasuken2@gmail.com>
2022-05-09 00:15:40 +0900

    fix contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,9 @@ module.exports = {
             div: {
               wordBreak: 'break-all',
             },
+            code: {
+              wordBreak: 'break-all',
+            },
           },
         },
       }),
